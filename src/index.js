ff6c92d6318e5af7198d12ae39bdfe4c005bfab5
Anton Khorev <tony29@yandex.ru>
2015-08-31 19:21:06 +0300

    run popup

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
+/*
 function htmlEncode(value) {
 	return value.toString()
 		.replace(/&/g,'&amp;').replace(/"/g,'&quot;').replace(/'/g,'&#39;')
 		.replace(/</g,'&lt;').replace(/>/g,'&gt;')
 	; // https://github.com/emn178/js-htmlencode/blob/master/src/htmlencode.js
 }
+*/
 
 function generateCode() {
-	return htmlEncode([
+	return [
 		"<!DOCTYPE html>",
 		"<html lang='en'>",
 		"<head>",
@@ -17,12 +19,28 @@ function generateCode() {
 		"Generated code!",
 		"</body>",
 		"</html>",
-	].join("\n"));
+	].join("\n");
+}
+
+function getHtmlDataUri(html) {
+	// with base64: https://developer.mozilla.org/en-US/docs/Web/API/WindowBase64/btoa
+	//return "data:text/html;charset=utf-8;base64,"+window.btoa(unescape(encodeURIComponent(str)));
+	// without base64: https://en.wikipedia.org/wiki/Data_URI_scheme
+	return 'data:text/html;charset=utf-8,'+encodeURIComponent(html);
 }
 
 $(function(){
 	$('.webgl-starter').each(function(){
 		var container=$(this);
-		container.html("<code><pre>"+generateCode()+"</code></pre>");
+		var code;
+		//container.html("<code><pre>"+generateCode()+"</code></pre>");
+		container.empty().append(
+			$("<code>").append(code=$("<pre>").text(generateCode()))
+		).append(
+			$("<button type='button'>Run</button>").click(function(){
+				//window.open("data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAUAAAAFCAYAAACNbyblAAAAHElEQVQI12P4//8/w38GIAXDIBKE0DHxgljNBAAO9TXL0Y4OHwAAAABJRU5ErkJggg==","generatedCode");
+				window.open(getHtmlDataUri(code.text()),"generatedCode");
+			})
+		);
 	});
 });
