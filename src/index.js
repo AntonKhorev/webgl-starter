b39c0e66091fafe0cf50a0ac27edaffc2982c617
Anton Khorev <tony29@yandex.ru>
2015-08-31 18:30:02 +0300

    simple generated code

--- src/index.js
+++ src/index.js
@@ -1,6 +1,28 @@
+function htmlEncode(value) {
+	return value.toString()
+		.replace(/&/g,'&amp;').replace(/"/g,'&quot;').replace(/'/g,'&#39;')
+		.replace(/</g,'&lt;').replace(/>/g,'&gt;')
+	; // https://github.com/emn178/js-htmlencode/blob/master/src/htmlencode.js
+}
+
+function generateCode() {
+	return htmlEncode([
+		"<!DOCTYPE html>",
+		"<html lang='en'>",
+		"<head>",
+		"<meta charset='utf-8' />",
+		"<title>Generated code</title>",
+		"</head>",
+		"<body>",
+		"Generated code!",
+		"</body>",
+		"</html>",
+	].join("\n"));
+}
+
 $(function(){
 	$('.webgl-starter').each(function(){
 		var container=$(this);
-		container.html("Do something");
+		container.html("<code><pre>"+generateCode()+"</code></pre>");
 	});
 });
