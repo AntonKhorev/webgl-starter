671aa8ee3793bbc2d6e9a42d354da89160fc296f
Anton Khorev <tony29@yandex.ru>
2015-11-24 13:49:49 +0300

    elements option for gasket - done

--- src/shapes.js
+++ src/shapes.js
@@ -136,10 +136,14 @@ var LodShape=function(elementIndexBits,shaderType,lod){
 };
 LodShape.prototype=Object.create(Shape.prototype);
 LodShape.prototype.constructor=LodShape;
-// abstract LodShape.prototype.getDistinctVertexCount=function(lodSymbol){}; // # of vertices when usesElements()
-// abstract LodShape.prototype.getTotalVertexCount=function(lodSymbol){}; // # of elements when usesElements(), otherwise # of vertices
+// abstract LodShape.prototype.getDistinctVertexCount=function(lodSymbol){}; // # of distinct vertices where one vertex can be shared between different faces and output primitives
+// abstract LodShape.prototype.getFaceVertexCount=function(lodSymbol){}; // # of distinct (vertex,face) pairs that still can be shared between output primitives
+// abstract LodShape.prototype.getTotalVertexCount=function(lodSymbol){}; // # of vertices in output primitives = # of elements when element arrays are in use
 // abstract LodShape.prototype.writeStoreShape=function(c,cv){};
 LodShape.prototype.writeArraysAndBufferData=function(debugArrays,c,cv){
+	var nVerticesFn = this.shaderType=='face'
+		? this.getFaceVertexCount
+		: this.getDistinctVertexCount;
 	var lines=new Lines;
 	if (this.lod.changes) {
 		lines.a(
@@ -148,7 +152,7 @@ LodShape.prototype.writeArraysAndBufferData=function(debugArrays,c,cv){
 		);
 		if (this.usesElements()) {
 			lines.a(
-				"var nMaxVertices="+this.getDistinctVertexCount("maxShapeLod")+";",
+				"var nMaxVertices="+nVerticesFn("maxShapeLod")+";",
 				"var vertices=new Float32Array(nMaxVertices*"+this.getNumbersPerVertex()+");",
 				"var nMaxElements="+this.getTotalVertexCount("maxShapeLod")+";",
 				"var elements=new "+this.getElementIndexJsArray()+"(nMaxElements);",
@@ -156,7 +160,7 @@ LodShape.prototype.writeArraysAndBufferData=function(debugArrays,c,cv){
 				"var shapeLod,nVertices,nElements;",
 				"function storeShape(newShapeLod) {",
 				"	shapeLod=newShapeLod;",
-				"	nVertices="+this.getDistinctVertexCount("shapeLod")+";",
+				"	nVertices="+nVerticesFn("shapeLod")+";",
 				"	nElements="+this.getTotalVertexCount("shapeLod")+";"
 			);
 		} else {
@@ -176,7 +180,7 @@ LodShape.prototype.writeArraysAndBufferData=function(debugArrays,c,cv){
 		);
 		if (this.usesElements()) {
 			lines.a(
-				"var nVertices="+this.getDistinctVertexCount("shapeLod")+";",
+				"var nVertices="+nVerticesFn("shapeLod")+";",
 				"var vertices=new Float32Array(nVertices*"+this.getNumbersPerVertex()+");",
 				"var nElements="+this.getTotalVertexCount("shapeLod")+";",
 				"var elements=new "+this.getElementIndexJsArray()+"(nElements);",
@@ -359,6 +363,9 @@ Gasket.prototype.constructor=Gasket;
 Gasket.prototype.getDistinctVertexCount=function(lodSymbol){
 	return "(Math.pow(3,"+lodSymbol+"+1)+3)/2";
 };
+Gasket.prototype.getFaceVertexCount=function(lodSymbol){
+	return "Math.pow(3,"+lodSymbol+")*3";
+};
 Gasket.prototype.getTotalVertexCount=function(lodSymbol){
 	return "Math.pow(3,"+lodSymbol+")*3";
 };
@@ -370,8 +377,16 @@ Gasket.prototype.writeStoreShapeWithElements=function(c,cv){
 			"vertices[nextIndexIntoVertices++]=p[1];"
 		);
 		if (c) {
+			if (this.shaderType=='face') {
+				lines.a(
+					"var c=colors[nextIndexIntoColors];"
+				);
+			} else {
+				lines.a(
+					"var c=colors[nextElement%colors.length];"
+				);
+			}
 			lines.a(
-				"var c=colors[nextElement%colors.length];",
 				"vertices[nextIndexIntoVertices++]=c[0];",
 				"vertices[nextIndexIntoVertices++]=c[1];",
 				"vertices[nextIndexIntoVertices++]=c[2];"
@@ -383,6 +398,82 @@ Gasket.prototype.writeStoreShapeWithElements=function(c,cv){
 		return lines.wrap(
 			"function pushVertex(p) {",
 			"}"
+		);
+	}
+	function writeTriangle() {
+		var lines=new Lines;
+		lines.a(
+			"if (depth<=0) {"
+		);
+		if (this.shaderType=='face') {
+			lines.a(
+				"	pushElement(pushVertex(p0));",
+				"	pushElement(pushVertex(p1));",
+				"	pushElement(pushVertex(p2));",
+				"	nextIndexIntoColors=(nextIndexIntoColors+1)%colors.length;"
+			);
+		} else {
+			lines.a(
+				"	if (es[0]===null) es[0]=pushVertex(p0);",
+				"	if (es[1]===null) es[1]=pushVertex(p1);",
+				"	if (es[2]===null) es[2]=pushVertex(p2);",
+				"	pushElement(es[0]);",
+				"	pushElement(es[1]);",
+				"	pushElement(es[2]);",
+				"	return es;"
+			);
+		}
+		lines.a(
+			"} else {",
+			"	var p01=mix(p0,p1);",
+			"	var p12=mix(p1,p2);",
+			"	var p20=mix(p2,p0);"
+		);
+		if (this.shaderType=='face') {
+			lines.a(
+				"	var es0=triangle(depth-1,p0,p01,p20);",
+				"	var es1=triangle(depth-1,p1,p12,p01);",
+				"	var es2=triangle(depth-1,p2,p20,p12);"
+			);
+		} else {
+			lines.a(
+				"	var es0=triangle(depth-1,p0,p01,p20,[es[0],null,null]);",
+				"	var es1=triangle(depth-1,p1,p12,p01,[es[1],null,es0[1]]);",
+				"	var es2=triangle(depth-1,p2,p20,p12,[es[2],es0[2],es1[1]]);",
+				"	return [es0[0],es1[0],es2[0]];"
+			);
+		}
+		lines.a(
+			"}"
+		);
+		if (this.shaderType=='face') {
+			return lines.wrap(
+				"function triangle(depth,p0,p1,p2) {",
+				"}"
+			);
+		} else {
+			return lines.wrap(
+				"function triangle(depth,p0,p1,p2,es) {",
+				"}"
+			);
+		}
+	}
+	function writeInitialTriangleCall() {
+		var lines=new Lines;
+		lines.a(
+			"shapeLod,",
+			"[-Math.sin(0/3*Math.PI),Math.cos(0/3*Math.PI)],",
+			"[-Math.sin(2/3*Math.PI),Math.cos(2/3*Math.PI)],",
+			"[-Math.sin(4/3*Math.PI),Math.cos(4/3*Math.PI)]"
+		);
+		if (this.shaderType!='face') {
+			lines.t(
+				",","[null,null,null]"
+			);
+		}
+		return lines.wrap(
+			"triangle(",
+			");"
 		);
 	}
 	var lines=new Lines;
@@ -401,13 +492,18 @@ Gasket.prototype.writeStoreShapeWithElements=function(c,cv){
 			"	[1.0, 1.0, 0.0],",
 			"];"
 		);
+		if (this.shaderType=='face') {
+			lines.a(
+				"var nextIndexIntoColors=0;"
+			);
+		}
 	} else {
 		lines.a(
 			"// p = position, e = element, es = elements"
 		);
 	}
 	lines.a(
-		writePushVertex(),
+		writePushVertex.call(this),
 		"function pushElement(e) {",
 		"	elements[nextIndexIntoElements++]=e;",
 		"}",
@@ -417,32 +513,8 @@ Gasket.prototype.writeStoreShapeWithElements=function(c,cv){
 		"		(pa[1]+pb[1])/2,",
 		"	];",
 		"}",
-		"function triangle(depth,p0,p1,p2,es) {",
-		"	if (depth<=0) {",
-		"		if (es[0]===null) es[0]=pushVertex(p0);",
-		"		if (es[1]===null) es[1]=pushVertex(p1);",
-		"		if (es[2]===null) es[2]=pushVertex(p2);",
-		"		pushElement(es[0]);",
-		"		pushElement(es[1]);",
-		"		pushElement(es[2]);",
-		"		return es;",
-		"	} else {",
-		"		var p01=mix(p0,p1);",
-		"		var p12=mix(p1,p2);",
-		"		var p20=mix(p2,p0);",
-		"		var es0=triangle(depth-1,p0,p01,p20,[es[0],null,null]);",
-		"		var es1=triangle(depth-1,p1,p12,p01,[es[1],null,es0[1]]);",
-		"		var es2=triangle(depth-1,p2,p20,p12,[es[2],es0[2],es1[1]]);",
-		"		return [es0[0],es1[0],es2[0]];",
-		"	}",
-		"}",
-		"triangle(",
-		"	shapeLod,",
-		"	[-Math.sin(0/3*Math.PI),Math.cos(0/3*Math.PI)],",
-		"	[-Math.sin(2/3*Math.PI),Math.cos(2/3*Math.PI)],",
-		"	[-Math.sin(4/3*Math.PI),Math.cos(4/3*Math.PI)],",
-		"	[null,null,null]",
-		");"
+		writeTriangle.call(this),
+		writeInitialTriangleCall.call(this)
 	);
 	return lines;
 }
