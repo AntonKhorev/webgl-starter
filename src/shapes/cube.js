d24ab922af011457d677da9ab5256db4add76419
Anton Khorev <tony29@yandex.ru>
2015-11-24 19:51:57 +0300

    elements option for cube - elements with face data

--- src/shapes/cube.js
+++ src/shapes/cube.js
@@ -56,62 +56,57 @@ Cube.prototype.writeArrays=function(c,cv){
 		" 0.0, 1.0, 1.0,",
 	];
 	var cubeFaceVertices=[
-		[4, 6, 0, 0, 6, 2],
-		[1, 3, 5, 5, 3, 7],
-		[0, 1, 4, 4, 1, 5],
-		[2, 6, 3, 3, 6, 7],
-		[0, 2, 1, 1, 2, 3],
-		[5, 7, 4, 4, 7, 6],
+		[4, 6, 0, 2],
+		[1, 3, 5, 7],
+		[0, 1, 4, 5],
+		[2, 6, 3, 7],
+		[0, 2, 1, 3],
+		[5, 7, 4, 6],
 	];
-
+	var quadToTriangleMap=[0, 1, 2, 2, 1, 3];
+	var vertexLines=new Lines;
+	vertexLines.a("// x    y    z");
+	if (this.shaderType=='light') {
+		vertexLines.t("  n.x  n.y  n.z");
+	}
+	if (c) {
+		vertexLines.t("    r    g    b");
+	}
 	if (this.shaderType=='face' || this.shaderType=='light') {
 		// elements with face data
-		var n=this.shaderType=='light';
-		return new Lines(
-			"var vertices=new Float32Array([",
-			"	// x    y    z "+(n?" n.x  n.y  n.z ":"   r    g    b"),
-			"	-0.5,-0.5,-0.5,"+(n?"-1.0, 0.0, 0.0,":" 1.0, 0.0, 0.0,")+" // left face",
-			"	-0.5,-0.5,+0.5,"+(n?"-1.0, 0.0, 0.0,":" 1.0, 0.0, 0.0,"),
-			"	-0.5,+0.5,-0.5,"+(n?"-1.0, 0.0, 0.0,":" 1.0, 0.0, 0.0,"),
-			"	-0.5,+0.5,+0.5,"+(n?"-1.0, 0.0, 0.0,":" 1.0, 0.0, 0.0,"),
-			"	+0.5,-0.5,-0.5,"+(n?"+1.0, 0.0, 0.0,":" 0.0, 1.0, 0.0,")+" // right face",
-			"	+0.5,+0.5,-0.5,"+(n?"+1.0, 0.0, 0.0,":" 0.0, 1.0, 0.0,"),
-			"	+0.5,-0.5,+0.5,"+(n?"+1.0, 0.0, 0.0,":" 0.0, 1.0, 0.0,"),
-			"	+0.5,+0.5,+0.5,"+(n?"+1.0, 0.0, 0.0,":" 0.0, 1.0, 0.0,"),
-			"	-0.5,-0.5,-0.5,"+(n?" 0.0,-1.0, 0.0,":" 1.0, 1.0, 0.0,")+" // bottom face",
-			"	+0.5,-0.5,-0.5,"+(n?" 0.0,-1.0, 0.0,":" 1.0, 1.0, 0.0,"),
-			"	-0.5,-0.5,+0.5,"+(n?" 0.0,-1.0, 0.0,":" 1.0, 1.0, 0.0,"),
-			"	+0.5,-0.5,+0.5,"+(n?" 0.0,-1.0, 0.0,":" 1.0, 1.0, 0.0,"),
-			"	-0.5,+0.5,-0.5,"+(n?" 0.0,+1.0, 0.0,":" 0.0, 0.0, 1.0,")+" // top face",
-			"	-0.5,+0.5,+0.5,"+(n?" 0.0,+1.0, 0.0,":" 0.0, 0.0, 1.0,"),
-			"	+0.5,+0.5,-0.5,"+(n?" 0.0,+1.0, 0.0,":" 0.0, 0.0, 1.0,"),
-			"	+0.5,+0.5,+0.5,"+(n?" 0.0,+1.0, 0.0,":" 0.0, 0.0, 1.0,"),
-			"	-0.5,-0.5,-0.5,"+(n?" 0.0, 0.0,-1.0,":" 1.0, 0.0, 1.0,")+" // back face",
-			"	-0.5,+0.5,-0.5,"+(n?" 0.0, 0.0,-1.0,":" 1.0, 0.0, 1.0,"),
-			"	+0.5,-0.5,-0.5,"+(n?" 0.0, 0.0,-1.0,":" 1.0, 0.0, 1.0,"),
-			"	+0.5,+0.5,-0.5,"+(n?" 0.0, 0.0,-1.0,":" 1.0, 0.0, 1.0,"),
-			"	-0.5,-0.5,+0.5,"+(n?" 0.0, 0.0,+1.0,":" 0.0, 1.0, 1.0,")+" // front face",
-			"	+0.5,-0.5,+0.5,"+(n?" 0.0, 0.0,+1.0,":" 0.0, 1.0, 1.0,"),
-			"	-0.5,+0.5,+0.5,"+(n?" 0.0, 0.0,+1.0,":" 0.0, 1.0, 1.0,"),
-			"	+0.5,+0.5,+0.5,"+(n?" 0.0, 0.0,+1.0,":" 0.0, 1.0, 1.0,"),
-			"]);",
-			"var nElements=36;",
-			"var elements=new "+this.getElementIndexJsArray()+"([",
-			"	 0,  1,  2,  2,  1,  3, // left face",
-			"	 4,  5,  6,  6,  5,  7, // right face",
-			"	 8,  9, 10, 10,  9, 11, // bottom face",
-			"	12, 13, 14, 14, 13, 15, // top face",
-			"	16, 17, 18, 18, 17, 19, // back face",
-			"	20, 21, 22, 22, 21, 23, // front face",
-			"]);"
-		);
+		if (this.usesElements()) {
+			for (var i=0;i<nCubeFaces;i++) {
+				cubeFaceVertices[i].forEach(function(j,k){
+					vertexLines.a(cubeVertexPositions[j]);
+					if (this.shaderType=='light') {
+						vertexLines.t(cubeFaceNormals[i]);
+					} else {
+						vertexLines.t(cubeFaceColors[i]);
+					}
+					if (k==0) {
+						vertexLines.t(" // "+cubeFaceNames[i]+" face");
+					}
+				},this);
+			}
+			return new Lines(
+				vertexLines.wrap(
+					"var vertices=new Float32Array([",
+					"]);"
+				),
+				"var nElements=36;",
+				"var elements=new "+this.getElementIndexJsArray()+"([",
+				"	 0,  1,  2,  2,  1,  3, // left face",
+				"	 4,  5,  6,  6,  5,  7, // right face",
+				"	 8,  9, 10, 10,  9, 11, // bottom face",
+				"	12, 13, 14, 14, 13, 15, // top face",
+				"	16, 17, 18, 18, 17, 19, // back face",
+				"	20, 21, 22, 22, 21, 23, // front face",
+				"]);"
+			);
+		} else {
+		}
 	} else {
 		// no face data
-		var vertexLines=new Lines;
-		vertexLines.a("// x    y    z");
-		if (c) {
-			vertexLines.t("    r    g    b");
-		}
 		if (this.usesElements()) {
 			for (var i=0;i<nCubeVertices;i++) {
 				vertexLines.a(cubeVertexPositions[i]);
@@ -121,7 +116,11 @@ Cube.prototype.writeArrays=function(c,cv){
 			}
 			var elementLines=new Lines;
 			for (var i=0;i<nCubeFaces;i++) {
-				elementLines.a(cubeFaceVertices[i].join(", ")+", // "+cubeFaceNames[i]+" face");
+				elementLines.a("");
+				quadToTriangleMap.forEach(function(j){
+					elementLines.t(cubeFaceVertices[i][j]+", ");
+				});
+				elementLines.t("// "+cubeFaceNames[i]+" face");
 			}
 			return new Lines(
 				vertexLines.wrap(
@@ -136,10 +135,11 @@ Cube.prototype.writeArrays=function(c,cv){
 			);
 		} else {
 			for (var i=0;i<nCubeFaces;i++) {
-				cubeFaceVertices[i].forEach(function(j,k){
-					vertexLines.a(cubeVertexPositions[j]);
+				quadToTriangleMap.forEach(function(j,k){
+					var v=cubeFaceVertices[i][j];
+					vertexLines.a(cubeVertexPositions[v]);
 					if (c) {
-						vertexLines.t(cubeVertexColors[j]);
+						vertexLines.t(cubeVertexColors[v]);
 					}
 					if (k==0) {
 						vertexLines.t(" // "+cubeFaceNames[i]+" face");
