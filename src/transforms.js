97cb137baa1a7bead00407e43c4acc1eefd51888
Anton Khorev <tony29@yandex.ru>
2016-01-13 00:03:26 +0300

    getting ready to implement rotation component maps

--- src/transforms.js
+++ src/transforms.js
@@ -10,17 +10,23 @@ class Transforms extends Feature {
 	constructor(options) {
 		super();
 		this.options=options;
-		const dims=['x','y','z'];
+		this.vectors=[]; // like features, but vectors have additional methods that don't follow feature interfaces
+		const possibleComponents=['x','y','z'];
 		const rotate=[[],[],[]];
-		options.model.forEach(tr=>{
+		options.model.forEach((tr,iTr)=>{
+			const trData={
+				transform: tr.value,
+				index: iTr,
+			};
 			if (tr.type=='rotate.x') {
-				rotate[0].push(tr.value);
+				rotate[0].push(trData);
 			} else if (tr.type=='rotate.y') {
-				rotate[1].push(tr.value);
+				rotate[1].push(trData);
 			} else if (tr.type=='rotate.z') {
-				rotate[2].push(tr.value);
+				rotate[2].push(trData);
 			}
 		});
+		const transformSequence=new Array(options.model.length); // [{vector,component}]
 		const nLayers=Math.max.apply(null,rotate.map(r=>r.length));
 		for (let i=0;i<nLayers;i++) {
 			let isInGap=false;
@@ -41,29 +47,37 @@ class Transforms extends Feature {
 					}
 				}
 			}
-			const varName='rotate'+(nLayers>1?i:'');
-			const htmlName='transforms.model.rotate'+(nLayers>1?'.'+i:'');
-			if (isStraight) {
-				const vector=new GlslVector('transforms.model.rotate',fixOptHelp.makeCollection(
-					rotate.map(r=>r[i]).slice(0,lastStraightIndex+1),
-					dims.slice(0,lastStraightIndex+1),
-					Options
-				));
-				vector.varName=varName;
-				vector.htmlName=htmlName;
+			const makeVector=(values,names)=>{
+				const vector=new GlslVector('transforms.model.rotate',fixOptHelp.makeCollection(values,names,Options));
+				vector.varName='rotate'+(nLayers>1?i:'');
+				vector.htmlName='transforms.model.rotate'+(nLayers>1?'.'+i:'');
 				this.features.push(vector);
+				this.vectors.push(vector);
+				return vector;
+			};
+			if (isStraight) {
+				const vector=makeVector(
+					rotate.slice(0,lastStraightIndex+1).map(r=>r[i].transform),
+					possibleComponents.slice(0,lastStraightIndex+1)
+				);
+				for (let j=0;j<=lastStraightIndex;j++) {
+					transformSequence[rotate[j][i].index]={
+						vector: vector,
+						component: possibleComponents[j]
+					};
+				}
 			} else {
 				for (let j=0;j<3;j++) {
 					const has=i<rotate[j].length;
 					if (has) {
-						const vector=new GlslVector('transforms.model.rotate',fixOptHelp.makeCollection(
-							[rotate[j][i]],
-							dims.slice(j,j+1),
-							Options
-						));
-						vector.varName=varName;
-						vector.htmlName=htmlName;
-						this.features.push(vector);
+						const vector=makeVector(
+							[rotate[j][i].transform],
+							possibleComponents.slice(j,j+1)
+						);
+						transformSequence[rotate[j][i].index]={
+							vector: vector,
+							component: possibleComponents[j]
+						};
 					}
 				}
 			}
@@ -76,6 +90,9 @@ class Transforms extends Feature {
 	// public:
 	getGlslVertexDeclarationLines(flatShape) {
 		const lines=new Lines;
+		this.vectors.forEach(vector=>{
+			lines.a(vector.getGlslDeclarationLines())
+		});
 		if (this.use2dTransform(flatShape)) {
 			lines.a("attribute vec2 position;");
 		} else {
