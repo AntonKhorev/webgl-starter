f70bc56a98d035ddf7239afe437c56b68bf796a7
Anton Khorev <tony29@yandex.ru>
2016-03-17 22:06:33 +0300

    shorter i18n expansions

--- src/i18n.js
+++ src/i18n.js
@@ -2,7 +2,7 @@
 
 let strings={
 	'options.canvas': "Canvas",
-	'options.canvas.{width,height}': "Canvas {width,height}",
+	'options.canvas.{width,height}': "Canvas {}",
 
 	'options.background': "Background",
 	'options.background.type': "Background type",
@@ -23,9 +23,9 @@ let strings={
 	'options.material.color': "Material color",
 	'options.material.color.{r,g,b,a}': "Material color {red,green,blue,alpha} component",
 	'options.material.color.{r,g,b,a}.speed': "Material color {red,green,blue,alpha} speed",
-	'options.material.{specular,diffuse,ambient}Color': "Material {specular,diffuse,ambient} color",
+	'options.material.{specular,diffuse,ambient}Color': "Material {} color",
 	'options.material.{specular,diffuse,ambient}Color.{r,g,b}': "{Specular,Diffuse,Ambient} color {red,green,blue} component",
-	'options.material.{specular,diffuse,ambient}Color.{r,g,b}.speed': "Material {specular,diffuse,ambient} color {red,green,blue,alpha} speed",
+	'options.material.{specular,diffuse,ambient}Color.{r,g,b}.speed': "Material {} color {red,green,blue,alpha} speed",
 
 	'options.light': "Light", // directional if on
 	'options.light.type': "Light type",
@@ -33,8 +33,8 @@ let strings={
 	'options.light.type.phong': "on with Phong reflections", // wp: Phong reflection model
 	'options.light.type.blinn': "on with Blinn–Phong reflections", // wp: Blinn–Phong shading model
 	'options.light.direction': "Light direction",
-	'options.light.direction.{x,y,z}': "Light direction {x,y,z} component",
-	'options.light.direction.{x,y,z}.speed': "Light direction {x,y,z} speed",
+	'options.light.direction.{x,y,z}': "Light direction {} component",
+	'options.light.direction.{x,y,z}.speed': "Light direction {} speed",
 
 	'options.shape': "Shape",
 	'options.shape.type': "Shape to draw",
@@ -46,7 +46,7 @@ let strings={
 	'options.shape.type.terrain': "diamond-square fractal terrain", // wp: Diamond-square algorithm
 	'options.shape.elements': "Element array",
 	'options.shape.elements.0': "not used",
-	'options.shape.elements.{8,16,32}': "with {8,16,32}-bit index",
+	'options.shape.elements.{8,16,32}': "with {}-bit index",
 	'options.shape.lod': "Shape detail level", // recursion depth for fractal shapes
 
 	'options.transforms': "Transforms",
@@ -54,9 +54,9 @@ let strings={
 	'options.transforms.projection.ortho': "orthogonal",
 	'options.transforms.projection.perspective': "perspective",
 	'options.transforms.model': "Model transform",
-	'options.transforms.model.rotate.{x,y,z}.add': "Add rotation around {x,y,z} axis",
-	'options.transforms.model.rotate.{x,y,z}': "Angle of rotation around {x,y,z} axis",
-	'options.transforms.model.rotate.{x,y,z}.speed': "Speed of rotation around {x,y,z} axis",
+	'options.transforms.model.rotate.{x,y,z}.add': "Add rotation around {} axis",
+	'options.transforms.model.rotate.{x,y,z}': "Angle of rotation around {} axis",
+	'options.transforms.model.rotate.{x,y,z}.speed': "Speed of rotation around {} axis",
 
 	'options.debug': "Debug options",
 	'options.debug.shaders': "Log shader compilation errors",
@@ -64,16 +64,11 @@ let strings={
 	'options.debug.inputs': "Log input values",
 	'options.debug.animations': "Log animated values",
 
-	'options.formatting': "Code formatting options",
-	'options.formatting.indent': "Indent",
-	'options.formatting.indent.tab': "tab",
-	'options.formatting.indent.{2,4,8}': "{2,4,8} spaces",
-
 	'options-output.inputs': "Input method",
 	'options-output.inputs.constant': "none",
 	'options-output.inputs.slider': "slider",
-	'options-output.inputs.mousemove{x,y}': "mouse {x,y} axis",
-	'options-output.inputs.gamepad{0,1,2,3}': "gamepad {0,1,2,3} axis",
+	'options-output.inputs.mousemove{x,y}': "mouse {} axis",
+	'options-output.inputs.gamepad{0,1,2,3}': "gamepad {} axis",
 	'options-output.range': "with range",
 	'options-output.reset': "Reset",
 	'options-output.addSpeed': "Add speed",
@@ -81,19 +76,15 @@ let strings={
 	'options-output.delete': "Delete transform",
 	'options-output.elements': "choosing 8- or 16-bit index may limit available shape detail levels",
 
-	'controls.type.mousemovex': "Move the mouse pointer horizontally over the canvas",
-	'controls.type.mousemovey': "Move the mouse pointer vertically over the canvas",
+	'controls.type.mousemove{x,y}': "Move the mouse pointer {horizontally,vertically} over the canvas",
 	'controls.to': "to update",
 
 	'units.pixel.a': "px",
-	'units.pixel.1': "pixel",
-	'units.pixel.2': "pixels",
+	'units.pixel.{1,2}': "pixel{,s}",
 	'units.°/second.a': "°/s",
-	'units.°/second.1': "degree per second",
-	'units.°/second.2': "degrees per second",
+	'units.°/second.{1,2}': "degree{,s} per second",
 	'units.second.a': "s",
-	'units.1/second.1': "unit per second", // "inverse second",
-	'units.1/second.2': "units per second", // "inverse seconds",
+	'units.1/second.{1,2}': "unit{,s} per second", // "inverse second(s)",
 }
 
 strings=require('crnx-base/i18n-expand-curly')(strings)
