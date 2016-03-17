657a4dfd6ccf1f36bcba924bbfe9872bbbbcccf2
Anton Khorev <tony29@yandex.ru>
2016-03-18 02:00:38 +0300

    more option tips

--- src/i18n.js
+++ src/i18n.js
@@ -26,28 +26,35 @@ let strings={
 	'options.material.{specular,diffuse,ambient}Color': "Material {} color",
 	'options.material.{specular,diffuse,ambient}Color.{r,g,b}': "{Specular,Diffuse,Ambient} color {red,green,blue} component",
 	'options.material.{specular,diffuse,ambient}Color.{r,g,b}.speed': "Material {} color {red,green,blue,alpha} speed",
+	'options-info.material.data.sda': "for use with Phong/Blinn–Phong shading models selected by <em>light type</em>",
 
-	'options.light': "Light", // directional if on
+	'options.light': "Light",
 	'options.light.type': "Light type",
 	'options.light.type.off': "off",
-	'options.light.type.phong': "on with Phong reflections", // wp: Phong reflection model
-	'options.light.type.blinn': "on with Blinn–Phong reflections", // wp: Blinn–Phong shading model
+	'options.light.type.phong': "on with Phong reflections",
+	'options.light.type.blinn': "on with Blinn–Phong reflections",
 	'options.light.direction': "Light direction",
 	'options.light.direction.{x,y,z}': "Light direction {} component",
 	'options.light.direction.{x,y,z}.speed': "Light direction {} speed",
+	'options-info.light.type.phong': "[[Shading#Directional_lighting|directional light]] with [[Phong reflection model]]",
+	'options-info.light.type.blinn': "[[Shading#Directional_lighting|directional light]] with [[Blinn–Phong shading model|Blinn–Phong reflection model]]",
 
 	'options.shape': "Shape",
 	'options.shape.type': "Shape to draw",
 	'options.shape.type.square': "square",
 	'options.shape.type.triangle': "triangle",
-	'options.shape.type.gasket': "Sierpinski gasket", // wp: Sierpinski triangle
+	'options.shape.type.gasket': "Sierpinski gasket",
 	'options.shape.type.cube': "cube",
-	'options.shape.type.hat': "Mexican hat function", // wp: Mexican hat wavelet
-	'options.shape.type.terrain': "diamond-square fractal terrain", // wp: Diamond-square algorithm
+	'options.shape.type.hat': "Mexican hat function",
+	'options.shape.type.terrain': "diamond-square fractal terrain",
 	'options.shape.elements': "Element array",
 	'options.shape.elements.0': "not used",
 	'options.shape.elements.{8,16,32}': "with {}-bit index",
 	'options.shape.lod': "Shape detail level", // recursion depth for fractal shapes
+	'options-info.shape.type.gasket': "[[Sierpinski triangle]]",
+	'options-info.shape.type.hat': "[[Mexican hat wavelet]]",
+	'options-info.shape.type.terrain': "generated with the [[Diamond-square algorithm|diamond-square algorithm]]",
+	'options-info.shape.elements.{8,16}': "choosing 8- or 16-bit index may limit available shape detail levels",
 
 	'options.transforms': "Transforms",
 	'options.transforms.projection': "Projection",
@@ -57,14 +64,14 @@ let strings={
 	'options.transforms.model.rotate.{x,y,z}.add': "Add rotation around {} axis",
 	'options.transforms.model.rotate.{x,y,z}': "Angle of rotation around {} axis",
 	'options.transforms.model.rotate.{x,y,z}.speed': "Speed of rotation around {} axis",
+	'options-info.transforms.projection.ortho': "[[Orthographic projection|orthographic projection]]",
+	'options-info.transforms.projection.perspective': "[[Transformation_matrix#Perspective_projection|perspective projection]]",
 
 	'options.debug': "Debug options",
 	'options.debug.shaders': "Log shader compilation errors",
 	'options.debug.arrays': "Log allocated array sizes",
 	'options.debug.inputs': "Log input values",
 	'options.debug.animations': "Log animated values",
-
-	'options-info.shape.elements.{8,16}': "choosing 8- or 16-bit index may limit available shape detail levels",
 
 	'options-output.inputs': "Input method",
 	'options-output.inputs.constant': "none",
@@ -89,6 +96,7 @@ let strings={
 }
 
 strings=require('crnx-base/i18n-expand-curly')(strings)
+strings=require('crnx-base/i18n-link-wikipedia')('en')(strings)
 strings=require('crnx-base/code-output-i18n')(strings)
 
 module.exports=require('crnx-base/i18n')({en:strings})
