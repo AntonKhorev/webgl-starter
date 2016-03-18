ab2febae0ae3a563e8a513eb2f42b69333d611af
Anton Khorev <tony29@yandex.ru>
2016-03-18 14:52:47 +0300

    started ru version

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,10 @@ const gulpTasks=require('crnx-build/gulp-tasks')
 
 gulpTasks(
 	gulp,
-	"WebGL starter code generator",
+	{
+		en: "WebGL starter code generator",
+		ru: "Генератор примеров WebGL",
+	},
 	['http://cdnjs.cloudflare.com/ajax/libs/highlight.js/8.7/styles/default.min.css'],
 	['http://cdnjs.cloudflare.com/ajax/libs/highlight.js/8.7/highlight.min.js'],
 	[require.resolve('crnx-base')]
