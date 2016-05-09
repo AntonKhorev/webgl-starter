735aecbe4022acbda9f04510165902e4359a0d7c
Anton Khorev <tony29@yandex.ru>
2016-05-09 04:35:33 +0300

    updated highlight.js

--- gulpfile.js
+++ gulpfile.js
@@ -9,7 +9,7 @@ gulpTasks(
 		en: "WebGL example generator",
 		ru: "Генератор примеров WebGL",
 	},
-	['http://cdnjs.cloudflare.com/ajax/libs/highlight.js/8.7/styles/default.min.css'],
-	['http://cdnjs.cloudflare.com/ajax/libs/highlight.js/8.7/highlight.min.js'],
+	['http://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.3.0/styles/default.min.css'],
+	['http://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.3.0/highlight.min.js'],
 	[require.resolve('crnx-base')]
 )
